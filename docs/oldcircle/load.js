efea6f8dc29210ff4a540384ea363c079eabffe1
pablofgaeta <pablofgaeta@gmail.com>
2019-08-19 23:56:27 -0700

    dynamically load content for mobile or desktop on home

---
+++ docs/oldcircle/load.js
@@ -0,0 +1,47 @@
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+let mob = isMobile.any();
+
+var head = document.getElementsByTagName('head')[0];
+var names = ["matterhome.js",
+            "mattercircle.js",
+            "boundary.js"];
+var js = [];
+
+if (!mob)
+{
+    for(var i=0; i<3;i++){
+        js[i] = document.createElement("script");
+        js[i].type = "text/javascript";
+        js[i].src = names[i];
+        head.appendChild(js[i]);
+    }
+}
+else
+{
+    var song = document.getElementById("song");
+    song.volume = .5;
+    song.play();
+}
+
+
+
