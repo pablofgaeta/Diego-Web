fdcff0c511712439122dfc40358e55e938218be0
pablofgaeta <pablofgaeta@gmail.com>
2019-08-24 15:15:53 -0700

    cleaned up recorded links

--- docs/oldcircle/sound.js
+++ docs/oldcircle/sound.js
@@ -4,25 +4,11 @@ let cnv;
 
 function preload(){
     sound = loadSound("../AUDIO/cherish\ love\ min.mp3");
-    // play2 = loadImage("../gifs/play.png");
-    // pause = loadImage("../gifs/pause.png");
-    // play = createImg("../gifs/play.png");
-    // pause = createImg("../gifs/pause.png");
 }
 function setup(){
-    cnv = createCanvas(windowWidth, windowHeight);
-    // play.style('display', 'none');
-    // pause.style('display', 'none');
-    // play.size(width/4, AUTO);
-    // pause.size(width/4, AUTO);
-    // play.mousePressed(playme);
-    // play.touchStarted(playme);
-    // pause.mousePressed(playme);
-    // pause.touchStarted(playme);
     sound.loop();
 }
 function draw(){
-    clear();
     // if(sound.isPlaying()){
     //     play.hide();
     //     pause.show();
@@ -33,36 +19,4 @@ function draw(){
     //     play.show();
     //     play.center();
     // }
-}
-
-function playme(){
-    if(!sound.isPlaying()){
-        sound.play();
-    }
-    else{
-        sound.pause();
-    }
-}
-// function mousePressed(){
-//     if(!sound.isPlaying()){
-//         sound.play();
-//     }
-//     else{
-//         sound.pause();
-//     }
-// }
-
-// function touchStarted(){
-//     if(!playing){
-//         sound.play();
-//         playing = true;
-//     }
-//     else{
-//         sound.pause();
-//         playing = false;
-//     }
-// }
-
-function windowResized(){
-    cnv = resizeCanvas(windowWidth, windowHeight);
 }
