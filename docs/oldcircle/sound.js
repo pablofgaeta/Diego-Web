1e4581e001deec38e663e85a0f9460f177b43115
pablofgaeta <pablofgaeta@gmail.com>
2019-08-24 10:03:09 -0700

    fast web home page music

--- docs/oldcircle/sound.js
+++ docs/oldcircle/sound.js
@@ -3,35 +3,36 @@ let play, pause, play2;
 let cnv;
 
 function preload(){
-    sound = loadSound("../AUDIO/cherish\ love.mp3");
+    sound = loadSound("../AUDIO/cherish\ love\ min.mp3");
     // play2 = loadImage("../gifs/play.png");
     // pause = loadImage("../gifs/pause.png");
-    play = createImg("../gifs/play.png");
-    pause = createImg("../gifs/pause.png");
+    // play = createImg("../gifs/play.png");
+    // pause = createImg("../gifs/pause.png");
 }
 function setup(){
     cnv = createCanvas(windowWidth, windowHeight);
-    play.style('display', 'none');
-    pause.style('display', 'none');
-    play.size(width/4, AUTO);
-    pause.size(width/4, AUTO);
-    play.mousePressed(playme);
-    play.touchStarted(playme);
-    pause.mousePressed(playme);
-    pause.touchStarted(playme);
+    // play.style('display', 'none');
+    // pause.style('display', 'none');
+    // play.size(width/4, AUTO);
+    // pause.size(width/4, AUTO);
+    // play.mousePressed(playme);
+    // play.touchStarted(playme);
+    // pause.mousePressed(playme);
+    // pause.touchStarted(playme);
+    sound.loop();
 }
 function draw(){
     clear();
-    if(sound.isPlaying()){
-        play.hide();
-        pause.show();
-        pause.center();
-    }
-    else{
-        pause.hide();
-        play.show();
-        play.center();
-    }
+    // if(sound.isPlaying()){
+    //     play.hide();
+    //     pause.show();
+    //     pause.center();
+    // }
+    // else{
+    //     pause.hide();
+    //     play.show();
+    //     play.center();
+    // }
 }
 
 function playme(){
